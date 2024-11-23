c2d9af194131d9282b99d73e67f97ac47ec7ce88
idosh <idoshziv@gmail.com>
2024-11-23 22:55:54 +0200

    fix: fixing broken test

--- common/visualizer.py
+++ common/visualizer.py
@@ -118,7 +118,8 @@ def visualize_algorithm_progress(graph, pos, steps,
 
     # Save the animation
     anim.save(output_file, writer='pillow')
-    patch_file_permissions(output_file)
+    if os.path.exists(output_file):
+        patch_file_permissions(output_file)
     # Only show the plot if not running in CI
     if not os.getenv("CI_RUN"):
         plt.show()
--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -5,7 +5,9 @@ import networkx as nx
 
 
 @patch('common.visualizer.FuncAnimation.save')
-def test_visualization(mock_save):
+@patch('common.visualizer.patch_file_permissions')  # Mock patch_file_permissions
+@patch('os.path.exists', return_value=True)  # Mock os.path.exists to avoid file system checks
+def test_visualization(mock_exists,  mock_patch_permissions, mock_save):
     # Mock graph and steps
     graph = nx.DiGraph()
     graph.add_edge(1, 2, weight=1)
@@ -13,7 +15,15 @@ def test_visualization(mock_save):
     pos = nx.spring_layout(graph)
     os.environ['CI_RUN'] = "True"
     # Call visualization
-    visualize_algorithm_progress(graph, pos, steps, output_file="test.gif")
+    visualize_algorithm_progress(graph, pos, steps,
+                                 title=f"MOCK Algorithm Animation",
+                                 output_file="test.gif")
 
-    # Assert save method was called
+    # Assert FuncAnimation.save was called correctly
     mock_save.assert_called_once_with("test.gif", writer='pillow')
+
+    # Assert patch_file_permissions was called with the correct argument
+    mock_patch_permissions.assert_called_once_with("test.gif")
+
+    # Assert that the file existence check was called
+    mock_exists.assert_called_once_with("test.gif")
