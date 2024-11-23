f8e2a56aeac960fa9db56f64a6197e20fd33dc13
idosh <idoshziv@gmail.com>
2024-11-23 21:09:21 +0200

    feat: Adding DFS logic and tests

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,4 +1,5 @@
 # Optionally, you can import common algorithms here for easier access
 from .dijkstra import dijkstra  # noqa: F401
 from .bfs import bfs  # noqa: F401
+from .dfs import dfs  # noqa: F401
 # from .bellman_ford import bellman_ford
---
+++ algorithms/dfs/README.md
@@ -0,0 +1,6 @@
+# Dfs
+        This is a new algorithm here!
+         ## Results
+![](./dfs_progress.gif)
+
+    
