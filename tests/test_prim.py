cdca71a8b22fd3d4fa7d494eece6042c8f236cef
Ido Ziv <52109442+ido123ziv@users.noreply.github.com>
2024-12-14 18:15:25 +0200

    feat: Prim 🖨️  (#8)
    * Add multiple python versions to check on
    
    * adding another test
    
    * Adding BFS Tests
    
    * fix: don't fail fast
    
    * Adding Build workflow
    
    * Adding more graphs
    
    * Adding a large graph
    
    * rollback change on visualizer.py
    
    * fix: fixing broken test
    
    * fix: fixing warnings
    
    * fix: naming
    
    * fix: multiple threads
    
    * fix: build cleanups
    
    * update workflow file
    
    * Update build.yml
    
    * chore[bot]: skip ci
    
    * feat: adding prim's algorithm
    
    * Adding another example for graph
    
    * chore[bot]: skip ci
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/test_prim.py
@@ -0,0 +1,32 @@
+
+import networkx as nx
+from algorithms.prim import run_algorithm
+
+def test_prim_algorithm():
+    graph = nx.Graph()
+    graph.add_weighted_edges_from([
+        (1, 2, 1),
+        (1, 3, 4),
+        (2, 3, 2),
+        (2, 4, 5),
+        (3, 4, 3)
+    ])
+    start_node = 1
+    mst_edges, total_weight, steps = run_algorithm(graph, start_node)
+    assert set(mst_edges) == {(1, 2), (2, 3), (3, 4)}
+    assert total_weight == 6
+
+def test_non_existent_start_node():
+    graph = nx.Graph()
+    graph.add_weighted_edges_from([
+        (1, 2, 1),
+        (1, 3, 4),
+        (2, 3, 2),
+        (2, 4, 5),
+        (3, 4, 3)
+    ])
+    start_node = 5
+    try:
+        run_algorithm(graph, start_node)
+    except ValueError as e:
+        assert str(e) == "Start node not found in the graph."
